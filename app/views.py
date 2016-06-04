4aadeb05ee35553820756e8562a194bde29b6471
Mike Barkas <mike@barkas.com>
2016-06-03 21:25:17 -0400

    Add template to route function.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world'
+    user = {'nickname': 'Joe'}
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
