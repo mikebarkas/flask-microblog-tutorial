9b96f01f7a45aaec26540779851e2eb4a13695d7
Mike Barkas <mike@barkas.com>
2016-06-03 21:36:19 -0400

    Setting example posts list in route function.

--- app/views.py
+++ app/views.py
@@ -5,6 +5,21 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Joe'}
+    posts = [
+        {
+            'author': {'nickname': 'Joe'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Billy'},
+            'body': 'The Avengers movie was cool.'
+        },
+        {
+            'author': {'nickname': 'Dave'},
+            'body': 'Oh my goodness..'
+        }
+    ]
     return render_template('index.html',
                            title='Home',
-                           user=user)
+                           user=user,
+                           posts=posts)
